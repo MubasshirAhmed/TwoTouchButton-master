1757cb92d8af9f3989fcd61ff78f20877a8b54d3
MUBASSHIRAHMED\Mubasshir Ahmed <MubasshirAhmed26@gmail.com>
2018-05-26 22:04:31 +0600

    Initial commit

---
+++ TwoTouchButton/Assets/Scripts/TouchToMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchToMove : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.GetTouch(0).position.x < Screen.width / 2)
+            {
+                transform.Translate(Vector2.left * Time.deltaTime * 10);
+            }
+            else
+            {
+                transform.Translate(-Vector2.left * Time.deltaTime * 10);
+            }
+        }
+    }
+}
